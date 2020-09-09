43ab419b6c813e506390b20caa0b98fe5614c88e
bruce <bruce.wang@zerologix.com>
2020-09-09 21:31:23 +1000

    update local vuew project/

---
+++ src/components/Customised.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="customized-wrapper">
+
+        <div>
+            <h1> Section Title here</h1>
+
+            <p> 
+            {{real}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+    name:"customized",
+    data() {
+        return {
+            real:'funny'
+        };
+    }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import customized  from './components/Customised.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+createApp(customized).mount('#app')
